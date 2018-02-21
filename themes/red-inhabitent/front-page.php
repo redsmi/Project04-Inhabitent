632c239ba6d0a19faf172ea32cb3098cabc5e687
David Yin <davidyin92@hotmail.com>
2018-02-21 13:36:10 -0800

    Front page start on loop for shop-feed and journal-feed.

--- themes/red-inhabitent/front-page.php
+++ themes/red-inhabitent/front-page.php
@@ -14,8 +14,61 @@ get_header(); ?>
 
 				<?php get_template_part( 'template-parts/content', 'page' ); ?>
 
-			<?php endwhile; // End of the loop. ?>
+			<?php endwhile; // End of the loop that prints Home page content ?>
 
+			<section class="home-banner">
+			</section><!-- home-banner -->
+			<section class="shop-feed">
+				<h2>...Shop Stuff...</h2>
+				<?php
+					$args1 = array( 
+						'post_type' => 'post', 
+						'order' => 'ASC',
+						'posts_per_page' => '1'
+								);
+					$products = new WP_Query( $args1 ); // instantiate our object
+				?>
+				<?php if ( $products->have_posts() ) : ?>
+					<?php while ( $products->have_posts() ) : $products->the_post(); ?>
+						<?php the_title( '<h2 class="entry-title">', '</h2>' ); ?>
+					<?php endwhile; ?>
+					<?php wp_reset_postdata(); ?>
+				<?php else : ?>
+      				<h2>Nothing found!</h2>
+				<?php endif; ?>
+			</section><!-- shop-feed -->
+			
+			<section class-"journal-feed">
+				<h2>...Inhabitent Journal...</h2>
+				<?php
+					$args2 = array( 
+						'post_type' => 'post', 
+						'orderby' => 'date',
+						'posts_per_page' => '3'
+								);
+					$journal = new WP_Query( $args2 ); // instantiate our object
+				?>
+				<?php if ( $journal->have_posts() ) : ?>
+					<?php echo '<ul class="journal-list">';?>
+
+					<?php while ( $journal->have_posts() ) : $journal->the_post(); ?>
+						<?php the_title( '<h2 class="entry-title">', '</h2>' ); ?>
+						<?php if ( has_post_thumbnail() ) : ?>
+							<?php the_post_thumbnail( 'medium' ); ?>
+						<?php endif; ?>
+
+						<div class="entry-meta">
+							<?php red_starter_posted_on(); ?> / <?php comments_number( '0 Comments', '1 Comment', '% Comments' ); ?>
+						</div><!-- .entry-meta -->
+					<?php endwhile; ?>
+
+					<?php echo '</ul>';?>
+					<?php wp_reset_postdata(); ?>
+				<?php else : ?>
+      				<h2>Nothing found!</h2>
+				<?php endif; ?>
+			</section><!-- journal-feed -->
+			
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
