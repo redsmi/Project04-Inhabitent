8a67be6795567de0d776d3c0418873c5dc8a94c6
David Yin <davidyin92@hotmail.com>
2018-02-21 16:08:55 -0800

    Front page shop-feed CSS mostly done

--- themes/red-inhabitent/front-page.php
+++ themes/red-inhabitent/front-page.php
@@ -19,7 +19,6 @@ get_header(); ?>
 			<section class="home-banner">
 			</section><!-- home-banner -->
 			<section class="shop-feed">
-			<section class="product-info container">
                 <h2>Shop Stuff</h2>
                 <?php
                 $terms = get_terms( array(
@@ -28,26 +27,25 @@ get_header(); ?>
                 ) );
                 if ( ! empty( $terms ) ) :
 				?>
-                    <div class="product-type-blocks">
+                    <div class="product-type-wrapper">
                         <?php foreach ( $terms as $term ) :
                             ?>
-                            <div class="product-type-block-wrapper">
+                            <div class="product-type">
                                 <img src="<?php echo get_template_directory_uri() . '/images/' . $term->slug; ?>.svg"
                                      alt="<?php echo $term->name; ?>"/>
                                 <p><?php echo $term->description; ?></p>
                                 <p>
                                     <a href="<?php echo get_term_link( $term ); ?>"
-                                      class="btn"><?php echo $term->name; ?> Stuff</a>
+                                      class="product-button"><?php echo $term->name; ?> Stuff</a>
                                 </p>
                             </div>
                         <?php endforeach; ?>
                     </div>
                 <?php endif; ?>
-            </section>
 			</section><!-- shop-feed -->
 			
 			<section class="journal-feed">
-				<h2>...Inhabitent Journal...</h2>
+				<h2>Inhabitent Journal</h2>
 				<?php
 					$args2 = array( 
 						'post_type' => 'post', 
