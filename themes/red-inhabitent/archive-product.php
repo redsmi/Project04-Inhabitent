fdd0c24c9304647c95d0417462f4cd7ce1e00026
David Yin <davidyin92@hotmail.com>
2018-02-22 19:19:36 -0800

    Shop products page loop code.

--- themes/red-inhabitent/archive-product.php
+++ themes/red-inhabitent/archive-product.php
@@ -10,17 +10,57 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
-			<h1> Testing archive products php </h1>
-			<?php while ( have_posts() ) : the_post(); ?>
+<div id="primary" class="content-area">
+	<main id="main" class="site-main" role="main">
 
-				<?php get_template_part( 'template-parts/content', 'page' ); ?>
+		<section class="product-info-container">
+			<?php
+			$terms = get_terms ( array(
+				'taxonomy' => 'product-type',
+				'hide_empty' => 0,
+			));
+			if ( ! empty ($terms) ):
+			?>	
+				<div class="product-type-wrapper">
+					<?php foreach ( $terms as $term ):?>
+					<div class="product-type-blocks">
+						<p>
+							<a href="<?php echo get_term_link( $term ); ?>"
+							class="btn"><?php echo $term->name; ?></a>
+						</p>
+					</div>
+			<?php endforeach; ?>
+				</div><!-- product-type-wrapper -->
+			<?php endif; ?>
+		</section>
 
-			<?php endwhile; // End of the loop. ?>
+        <?php if ( have_posts() ) : ?>
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+            <header class="page-header">
+                <?php
+                    the_archive_title( '<h1 class="page-title">', '</h1>' );
+                ?>
+            </header><!-- .page-header -->
 
-<?php get_sidebar(); ?>
+            <?php /* Start the Loop */ ?>
+            <div class="products-wrapper">
+            <?php while ( have_posts() ) : the_post(); ?>
+
+                <?php get_template_part( 'template-parts/products-archives', 'none' ); ?>
+
+
+            <?php endwhile; ?>
+            </div>
+
+            <?php the_posts_navigation(); ?>
+
+        <?php else : ?>
+
+            <?php get_template_part( 'template-parts/content', 'none' ); ?>
+
+        <?php endif; ?>
+
+        </main><!-- #main -->
+    </div><!-- #primary -->
+
 <?php get_footer(); ?>
---
+++ themes/red-inhabitent/template-parts/products-archives.php
@@ -0,0 +1,14 @@
+<article id="post-<?php the_ID(); ?>" <?php post_class('shop-item'); ?>>
+ <header class="entry-header">
+   <?php if ( has_post_thumbnail() ) : ?>
+     <?php the_post_thumbnail( 'large' ); ?>
+   <?php endif; ?>
+
+   <?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">',
+   esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+
+   <?php echo CFS()->get( 'price' ); ?>
+
+ </header><!-- .entry-header -->
+
+</article><!-- #post-## -->
