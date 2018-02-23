f56425997bcec7074e6cd73a97cb4eb09cc4d582
David Yin <davidyin92@hotmail.com>
2018-02-23 14:04:15 -0800

    Project typography CSS. Search form static.

--- themes/red-inhabitent/header.php
+++ themes/red-inhabitent/header.php
@@ -29,8 +29,10 @@
 				</div><!-- .site-branding -->
 
 				<nav id="site-navigation" class="main-navigation" role="navigation">
+					<div class-"nav-container">
 					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html( 'Primary Menu' ); ?></button>
 					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
+					</div>
 					<div class="nav-search">
 						<?php get_search_form(); ?>
 					</div>
--- themes/red-inhabitent/searchform.php
+++ themes/red-inhabitent/searchform.php
@@ -1,5 +1,8 @@
 <form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
 	<fieldset>
+		<!-- <a href="#" class="search-toggle" aria-hidden="true">
+			<i class="fa fa-search"></i>
+		</a> -->
 		<label>
 			<input type="search" class="search-field" placeholder="SEARCH ..." value="<?php echo esc_attr( get_search_query() ); ?>" name="s" title="Search for:" />
 		</label>
