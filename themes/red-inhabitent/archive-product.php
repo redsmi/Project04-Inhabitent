931b3e1cbfaf765d761ad3a4fb691012308db918
David Yin <davidyin92@hotmail.com>
2018-02-26 12:41:48 -0800

    archive-products.php redid headings with <ul>. CSS changed earlier .archive to .archive.date

--- themes/red-inhabitent/archive-product.php
+++ themes/red-inhabitent/archive-product.php
@@ -13,34 +13,31 @@ get_header(); ?>
 <div id="primary" class="content-area">
 	<main id="main" class="site-main" role="main">
 
-		<section class="product-info-container">
+		<header class="page-header">
+			<h1>Shop Stuff</h1>
+		<ul class="product-types-list">
 			<?php
 			$terms = get_terms ( array(
 				'taxonomy' => 'product-type',
 				'hide_empty' => 0,
 			));
 			if ( ! empty ($terms) ):
-			?>	
-				<div class="product-type-wrapper">
-					<?php foreach ( $terms as $term ):?>
-					<div class="product-type-blocks">
-						<p>
-							<a href="<?php echo get_term_link( $term ); ?>"
-							class="btn"><?php echo $term->name; ?></a>
-						</p>
-					</div>
+			?>
+				<?php foreach ( $terms as $term ):?>
+				<li class="product-type">
+					<p>
+						<a href="<?php echo get_term_link( $term ); ?>"
+						class="btn"><?php echo $term->name; ?></a>
+					</p>
+				</li>
 			<?php endforeach; ?>
-				</div><!-- product-type-wrapper -->
 			<?php endif; ?>
-		</section>
+		</ul>
+		</header><!-- .page-header -->
 
         <?php if ( have_posts() ) : ?>
 
-            <header class="page-header">
-                <?php
-                    the_archive_title( '<h1 class="page-title">', '</h1>' );
-                ?>
-            </header><!-- .page-header -->
+			<!-- moved header -->
 
             <?php /* Start the Loop */ ?>
             <div class="products-wrapper">
