f7009ca250e86db81ed70d4a921ce8f2446162d5
David Yin <davidyin92@hotmail.com>
2018-02-26 14:53:48 -0800

    Shop flexbox start. Hyperlink on thumbnails.

--- themes/red-inhabitent/template-parts/products-archives.php
+++ themes/red-inhabitent/template-parts/products-archives.php
@@ -1,14 +1,19 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class('shop-item'); ?>>
- <header class="entry-header">
-   <?php if ( has_post_thumbnail() ) : ?>
-     <?php the_post_thumbnail( 'large' ); ?>
-   <?php endif; ?>
+  <!-- <header class="entry-header"> -->
+    <?php if ( has_post_thumbnail() ) : ?>
+      <div class="thumbnail">
+      <a href="<?php the_permalink(); ?>">
+        <?php the_post_thumbnail( 'large' ); ?>
+      </a>
+      </div>
+    <?php endif; ?>
 
-   <?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">',
-   esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+    <?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">',
+    esc_url( get_permalink() ) ), '</a></h2>' ); ?>
 
-   <?php echo CFS()->get( 'price' ); ?>
+    <p><?php echo CFS()->get( 'price' ); ?></p>
 
- </header><!-- .entry-header -->
+  <!-- </header> -->
+  <!-- .entry-header -->
 
 </article><!-- #post-## -->
