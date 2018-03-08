49a6b7b3fb9c0cb373d8e709dd08ea76e884f3eb
David Yin <davidyin92@hotmail.com>
2018-03-08 14:38:54 -0800

    Tidy footer and appropriate CSS

--- themes/red-inhabitent/about.php
+++ themes/red-inhabitent/about.php
@@ -16,19 +16,19 @@ get_header(); ?>
 						<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 					</header><!-- .entry-header -->
 					<div class="about-body">
-					<div class="entry-content">
-						<h2>Our Story</h2>
-						<?php echo CFS() -> get ('our_story'); ?>
-						<h2>Our Team</h2>
-						<?php echo CFS() -> get ('our_team'); ?>
+						<div class="entry-content">
+							<h2>Our Story</h2>
+							<?php echo CFS() -> get ('our_story'); ?>
+							<h2>Our Team</h2>
+							<?php echo CFS() -> get ('our_team'); ?>
 
-						<?php
-							wp_link_pages( array(
-								'before' => '<div class="page-links">' . esc_html( 'Pages:' ),
-								'after'  => '</div>',
-							) );
-						?>
-					</div><!-- .entry-content -->
+							<?php
+								wp_link_pages( array(
+									'before' => '<div class="page-links">' . esc_html( 'Pages:' ),
+									'after'  => '</div>',
+								) );
+							?>
+						</div><!-- .entry-content -->
 					</div>
 				</article><!-- #post-## -->
 			<?php endwhile; // End of the loop. ?>
--- themes/red-inhabitent/footer.php
+++ themes/red-inhabitent/footer.php
@@ -11,8 +11,7 @@
 
 			<footer id="colophon" class="site-footer testingfooter" role="contentinfo">
 				<div class="footer-blocks">
-					<div class="block1">
-						<div class="contact">
+					<div class="contact">
 							<h3>Contact Info</h3>
 							<p><i class="fa fa-envelope"></i><a href="mailto:info@inhabitent.com">info@inhabitent.com</a></p>
 							<p><i class="fas fa-mobile"></i><a href="tel:5553434567891">778-456-7891</a></p>
@@ -21,17 +20,14 @@
 								<span><i class="fab fa-twitter-square"></i></span>
 								<span><i class="fab fa-google-plus-square"></i></span>
 							</p>
-						</div><!-- .contact -->
 					</div>
-					<div class="block2">
-						<div class="business-hours">
+					<div class="business-hours">
 							<h3>Business Hours</h3>
 							<p><span class="day-of-week">Monday-Friday:</span> 9am to 5pm</p>
 							<p><span class="day-of-week">Saturday:</span> 10am to 2pm</p>
 							<p><span class="day-of-week">Sunday:</span> Closed</p>
-						</div><!-- .business-hours -->
 					</div>
-					<div class="block3">
+					<div class="footer-logo">
 						<div class="text-logo">
 							<a href="<?php esc_url(the_permalink()); ?>" rel="home">
 								<img src="<?php echo get_stylesheet_directory_uri(); ?>/images/inhabitent-logo-text.svg" alt="Inhabitent logo">
