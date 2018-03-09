803329cbf9cc642319e160a2142983fea2485d4e
David Yin <davidyin92@hotmail.com>
2018-03-09 14:44:46 -0800

    Organize SASS

--- themes/red-inhabitent/archive-adventures.php
+++ themes/red-inhabitent/archive-adventures.php
@@ -7,52 +7,44 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+<div id="primary" class="content-area">
+	<main id="main" class="site-main" role="main">
+		<header class="page-header">
+			<?php the_archive_title( '<h1 class="page-title">', '</h1>' ); ?>
+		</header><!-- .page-header -->
+		
+		<div class="adventures-grid-items">
+			<?php if ( have_posts() ) : ?>
+				<?php /* Start the Loop */ ?>
+				<?php while ( have_posts() ) : the_post(); ?>
 
-		<?php if ( have_posts() ) : ?>
+					<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+						<header class="entry-header">
+							<?php if ( has_post_thumbnail() ) : ?>
+								<?php the_post_thumbnail( 'large' ); ?>
+							<?php endif; ?>
 
-			<header class="page-header">
-				<?php
-					the_archive_title( '<h1 class="page-title">', '</h1>' );
-				?>
-			</header><!-- .page-header -->
+							<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
 
-			<?php /* Start the Loop */ ?>
-			<?php while ( have_posts() ) : the_post(); ?>
+							<?php if ( 'post' === get_post_type() ) : ?>
+							<div class="entry-meta">
+								<?php inhabitent_posted_on(); ?> / <?php comments_number( '0 Comments', '1 Comment', '% Comments' ); ?> / <?php inhabitent_posted_by(); ?>
+							</div><!-- .entry-meta -->
+							<?php endif; ?>
+						</header><!-- .entry-header -->
 
-				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-					<header class="entry-header">
-						<?php if ( has_post_thumbnail() ) : ?>
-							<?php the_post_thumbnail( 'large' ); ?>
-						<?php endif; ?>
-
-						<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
-
-						<?php if ( 'post' === get_post_type() ) : ?>
-						<div class="entry-meta">
-							<?php inhabitent_posted_on(); ?> / <?php comments_number( '0 Comments', '1 Comment', '% Comments' ); ?> / <?php inhabitent_posted_by(); ?>
-						</div><!-- .entry-meta -->
-						<?php endif; ?>
-					</header><!-- .entry-header -->
-
-					<div class="entry-content">
-						<a href="<?php esc_url(the_permalink()); ?>" class="read-entry">Read More →</a>
-						<!-- moved from index.php to here -->
-					</div><!-- .entry-content -->
-				</article><!-- #post-## -->
-
-			<?php endwhile; ?>
-
-			<?php the_posts_navigation(); ?>
-
-		<?php else : ?>
-
-			<?php get_template_part( 'template-parts/content', 'none' ); ?>
-
-		<?php endif; ?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
+						<div class="entry-content">
+							<a href="<?php esc_url(the_permalink()); ?>" class="read-entry">Read More →</a>
+							<!-- moved from index.php to here -->
+						</div><!-- .entry-content -->
+					</article><!-- #post-## -->
+					
+				<?php endwhile; ?>
+			<?php else : ?>
+				<?php get_template_part( 'template-parts/content', 'none' ); ?>
+			<?php endif; ?>
+		</div><!-- adventures-grid-items -->
+	</main><!-- #main -->
+</div><!-- #primary -->
 
 <?php get_footer(); ?>
